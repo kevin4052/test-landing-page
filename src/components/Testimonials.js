9762c1a425a7e1d13a77decdd35e13e6a6857c08
Kevin Hernandez <kevin.hernandez001@gmail.com>
2021-11-04 13:59:14 -0400

    testimonial slider

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
+import {
+    faChevronLeft,
+    faChevronRight,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import TestimonialCard from './TestimonialCard';
 
 export default function Testimonials() {
+    const [sliderTransform, setSliderTransform] = React.useState(0);
+
+    const handleClick = () => {
+        setSliderTransform(0);
+    };
     return (
         <Row className="testimonial">
             <Col>
@@ -12,10 +22,38 @@ export default function Testimonials() {
                         Simple innate summer fat appear basket his desire joy.
                     </p>
                 </Row>
-                <Row>
-                    <TestimonialCard />
-                    <TestimonialCard />
-                    <TestimonialCard />
+                <Row className="testimonial-body">
+                    <TestimonialCard
+                        style={{
+                            transform: `translate3d(${sliderTransform}px, 0px, 0px)`,
+                        }}
+                    />
+                    <TestimonialCard
+                        style={{
+                            transform: `translate3d(${sliderTransform}px, 0px, 0px)`,
+                        }}
+                    />
+                    <TestimonialCard
+                        style={{
+                            transform: `translate3d(${sliderTransform}px, 0px, 0px)`,
+                        }}
+                    />
+                </Row>
+                <Row className="testimonial-btn">
+                    <Col>
+                        <div>
+                            <FontAwesomeIcon
+                                icon={faChevronLeft}
+                                onClick={handleClick}
+                            />
+                        </div>
+                        <div>
+                            <FontAwesomeIcon
+                                icon={faChevronRight}
+                                onClick={handleClick}
+                            />
+                        </div>
+                    </Col>
                 </Row>
             </Col>
         </Row>
